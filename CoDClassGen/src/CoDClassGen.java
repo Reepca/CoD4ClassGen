2562b28330925109c6b0e6c9b8e569924c1b6c00
Reepca <anotherreepca@gmail.com>
2014-09-28 20:47:59 -0500

    Main Project Code
    Added basic template of a standard Processing sketch, as written using
    Eclipse, that will support being run as applet or application.

---
+++ CoDClassGen/src/CoDClassGen.java
@@ -0,0 +1,18 @@
+import processing.core.*;
+
+
+public class CoDClassGen extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main(new String[] {"CoDClassGen" });
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+
+}
